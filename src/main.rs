bdd9bafba49b7f4d5780080a3e44aa7c1ccf9fe6
Vitalii <86424813+VitalikMk@users.noreply.github.com>
2023-07-28 14:31:56 +0300

    2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
+use chrono::{DateTime, Local};
+
+enum Priority {
+    Low,
+    Medium,
+    High
+}
+
+
+impl Priority{
+    fn to_string(&self) -> String {
+        match self {
+            Priority::Low => "Low".to_owned(),
+            Priority::Medium => "Medium".to_owned(),
+            Priority::High => "High".to_owned(),
+        }
+    }
+}
 struct Task {
     name: String,
-    description: String
+    description: String,
+    priority: Priority,
+    add_time: DateTime<Local>
+}
+
+
+impl Task {
+    fn new(name: String, description: String, priority: Priority) -> Self {
+        Self { name, description, priority, add_time: Local::now() }
+    }
+
+
+    fn print_task(&self) {
+        println!("{} | {} | {}",
+                 self.name,
+                 self.priority.to_string(),
+                 self.add_time.format("%Y-%m-%d %H:%M:%S")
+        )
+    }
 }
 
 
