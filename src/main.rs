783f5c8c400891f9815b0c2157f6f8800106c31c
Vitalii <86424813+VitalikMk@users.noreply.github.com>
2023-07-28 16:02:43 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::format;
 use chrono::{DateTime, Local};
 
 enum Priority {
@@ -43,14 +44,14 @@ impl Task {
 struct TasksManager {
     tasks: Vec<Task>
 }
-
+//методи роботи з таск (виведення, додавання, видалення, знайти)
 impl TasksManager {
     fn new() -> Self {
         Self { tasks: vec![] }
     }
 
     fn print_tasks(&self) {
-        for task in self.tasks {
+        for task in &self.tasks {
             task.print_task();
         }
     }
@@ -58,6 +59,40 @@ impl TasksManager {
     fn add_task(&mut self, task: Task) {
         self.tasks.push(task);
     }
+
+    fn find_task(&self, name: &str) -> Option<usize> {
+        self.tasks.iter().position(|task| task.name == name)
+    }
+
+    fn remove_task(&mut self, name: &str) -> Result<String, String> {
+        if let Some(index) = self.find_task(name) {
+            self.tasks.remove(index);
+            Ok(format!("Task \"{}\" removed good", name))
+        } else {
+            Err(format!("Task with name \"{}\" doesn't exist", name))
+        }
+
+    }
+
+
+
+    fn edit_task(&mut self, name: String, updated_task: Task) -> Result<String, String> {
+        if let Some(index) = self.find_task(&name) {
+            match self.tasks.get_mut(index) {
+                None => Err("Error borrowing task".to_owned()),
+                Some(task) => {
+                    task.name = updated_task.name;
+                    task.description = updated_task.description;
+                    task.priority = updated_task.priority;
+                    Ok(format!("Task \"{}\" removed successfully", name))
+                }
+            }
+
+        } else {
+            Err(format!("Task with name \"{}\" doesn't exist", name))
+        }
+    }
+
 }
 
 fn main() {
