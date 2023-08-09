92d0235ab5d46b5d98b075674a22ed7c5754fe3e
Vitalii <86424813+VitalikMk@users.noreply.github.com>
2023-08-09 17:08:58 +0300

    123

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::ffi::c_int;
 use std::fmt::format;
+use std::io::Write;
 use chrono::{DateTime, Local};
+use serde::__private::de::Content::String;
 
 enum Priority {
     Low,
@@ -93,6 +96,73 @@ impl TasksManager {
         }
     }
 
+}
+struct ConsoleManager {
+    tasks_manager: TasksManager,
+    menu_options: Vec<String()>
+}
+
+impl ConsoleManager {
+
+    fn new() -> Self {
+        Self { tasks_manager: TasksManager::new(), menu_options: vec![
+            "Add task".to_owned(),
+            "Find task".to_owned(),
+            "Edit task".to_owned(),
+            "Remove task".to_owned(),
+            "Print task".to_owned(),
+            "Store tasks to file".to_owned(),
+            "Read tasks from file".to_owned()
+        ] }
+    }
+
+    fn print_menu(&self) {
+        for (index, menu_option) in self.menu_options.iter().enumerate() {
+            println!("{}. {}", index + 1, menu_option);
+        }
+    }
+
+    fn input(query: &str) -> std::io::Result<String> {
+        print!("{}", query);
+        std::io::stdout().flush()?;
+
+        let mut buffer = String::new();
+        std::io::stdin().read_line(&mut buffer)?;
+        Ok(buffer.trim().to_owned())
+    }
+
+    fn process_command(&self) {
+        match Self::input("Enter command index: ") {
+            Ok(command) => {
+                match command.as_str() {
+                    "1" => {
+                        self.tasks_manager.add_task()
+                    }
+                    "2" => {
+                        self.tasks_manager.find_task()
+                    }
+                    "3" => {
+                        self.tasks_manager.edit_task()
+                    }
+                    "4" => {
+                        self.tasks_manager.remove_task()
+                    }
+                    "5" => {
+                        self.tasks_manager.print_tasks()
+                    }
+                    "6" => {
+
+                    }
+                    "7" => {
+
+                    }
+
+                    _ => println!("Sho ty kurwa napisal, close programu chort")
+                }
+            }
+            Err(err) => println!("Error getting user input: {err}")
+        }
+    }
 }
 
 fn main() {
